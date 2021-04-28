c00ace308b6c2d42a1a7f440e62313b5153d85b6
kar655 <karol123zag@gmail.com>
2021-04-28 17:13:08 +0200

    Syntax highlighting

--- frama/helpers.py
+++ frama/helpers.py
@@ -1,6 +1,8 @@
+import re
+from pprint import pprint
 import subprocess
 
-from .models import File, User, Directory
+from .models import File, User, Directory, FileSection
 
 
 def init_database():
@@ -17,17 +19,79 @@ def focus_on_program_elements_helper(file: File) -> str:
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
 
-    print(f"stdout = {result.stdout}")
-    print(f"stderr = {result.stderr}")
+    # print(f"stdout = {result.stdout}")
+    # print(f"stderr = {result.stderr}")
+    #
+    # def parse_between_dashed_lines(process_output: str):
+    #     parsed = []
+    #     current = []
+    #     for line in process_output.splitlines(keepends=True):
+    #         if re.match(r"-+", line):
+    #             parsed.append(current)
+    #             current = []
+    #         else:
+    #             matches = re.search(r"line ([\d]+)", line)
+    #             if matches is not None:
+    #                 line_number = matches.group(1)
+    #                 print(f"============LINE NUMBER = {line_number}")
+    #
+    #             current.append(line)
+    #
+    #     if len(current) > 0:
+    #         parsed.append(current)
+    #
+    #     parsed_return = []
+    #
+    #     for section in parsed:
+    #         parsed_return.append(''.join(section))
+    #
+    #     return parsed_return
+    #
+    # parsed = parse_between_dashed_lines(result.stdout)
+    # for section in parsed:
+    #     print("SECTION:")
+    #     print(section)
 
     return result.stdout + result.stderr
 
 
 def read_file(file: File) -> str:
+    #         PROCEDURE = "Procedure", gettext_lazy("Procedure")
+    #         PROPERTY = "Property", gettext_lazy("Property")
+    #         LEMMA = "Lemma", gettext_lazy("Lemma")
+    #         ASSERTION = "Assertion", gettext_lazy("Assertion")
+    #         INVARIANT = "Invariant", gettext_lazy("Invariant")
+    #         PRECONDITION = "Precondition", gettext_lazy("Precondition")
+    #         POSTCONDITION = "Postcondition", gettext_lazy("Postcondition")
+    sections = {
+        "predicate": FileSection.SectionCategory.PROCEDURE,
+        "requires": FileSection.SectionCategory.PRECONDITION,
+        "ensures": FileSection.SectionCategory.POSTCONDITION,
+        "assert": FileSection.SectionCategory.ASSERTION,
+        "lemma": FileSection.SectionCategory.LEMMA,
+        "loop invariant": FileSection.SectionCategory.INVARIANT
+        # "logic real": FileSection.SectionCategory
+    }
+
     file.file_field.open("r")
-    lines = file.file_field.read()
+    # lines = file.file_field.read()
+    lines = "EMPTY"
+
+    result = []
+    for line in file.file_field.readlines():
+        match_results = re.search(r"@ +([a-z]+(?: [a-z]+)?)", line)
+        if match_results is not None:
+            match = match_results.group(1)
+            if match in sections:
+                result.append((sections[match], line))
+            else:
+                result.append((None, line))
+        else:
+            result.append((None, line))
+
     file.file_field.close()
-    return lines
+    pprint(result)
+    return result
 
 
 def get_current_user(session) -> User:
