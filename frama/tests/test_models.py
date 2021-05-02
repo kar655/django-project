ba6fdaf8982c4b8de7d091282b68702ffd0b8298
kar655 <karol123zag@gmail.com>
2021-05-03 00:39:49 +0200

    Add models tests

---
+++ frama/tests/test_models.py
@@ -0,0 +1,72 @@
+from datetime import datetime
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from ..models import Directory, File, FileSection
+
+
+class DirectoryModelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(username="user", password="password")
+        self.directory = Directory.objects.create(name="My Directory", user=self.user)
+
+    def test_str(self):
+        string = str(self.directory)
+        self.assertEqual(string, "Directory: My Directory")
+
+    def test_user_ownership(self):
+        self.assertEqual(self.directory.user, self.user)
+
+    def test_is_valid(self):
+        self.assertTrue(self.directory.is_valid)
+
+    def test_timestamp(self):
+        date_format = "%d/%m/%Y"
+        self.assertEqual(self.directory.timestamp.strftime(date_format), datetime.now().strftime(date_format))
+
+
+class FileModelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(username="user", password="password")
+        self.directory = Directory.objects.create(name="My Directory", user=self.user)
+        self.file = File.objects.create(name="My File", user=self.user, parent_directory=self.directory)
+
+    def test_str(self):
+        string = str(self.file)
+        self.assertEqual(string, "File: My File")
+
+    def test_user_ownership(self):
+        self.assertEqual(self.file.user, self.user)
+
+    def test_is_valid(self):
+        self.assertTrue(self.file.is_valid)
+
+    def test_timestamp(self):
+        date_format = "%d/%m/%Y"
+        self.assertTrue(self.file.timestamp.strftime(date_format) == datetime.now().strftime(date_format))
+
+
+class FileSectionModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="user", password="password")
+        self.directory = Directory.objects.create(name="My Directory", user=self.user)
+        self.file = File.objects.create(name="My File", user=self.user, parent_directory=self.directory)
+        self.file_section = FileSection.objects.create(file_referred=self.file, user=self.user,
+                                                       category=FileSection.SectionCategory.LEMMA,
+                                                       status=FileSection.SectionStatus.INVALID)
+
+    def test_str(self):
+        string = str(self.file_section)
+        self.assertEqual(string, "Lemma section of file My File")
+
+    def test_user_ownership(self):
+        self.assertEqual(self.file_section.user, self.user)
+
+    def test_is_valid(self):
+        self.assertTrue(self.file_section.is_valid)
+
+    def test_timestamp(self):
+        date_format = "%d/%m/%Y"
+        self.assertTrue(self.file_section.timestamp.strftime(date_format) == datetime.now().strftime(date_format))
